04aba308bf42cc13645b16ae35ee2c41c90d0e87
Jin Hao <limjinhao@gmail.com>
2022-10-01 21:35:02 +0800

    remove instagram

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -1,5 +1,5 @@
 import { IconType } from 'react-icons';
-import { SiGithub, SiLinkedin, SiInstagram, SiGmail } from 'react-icons/si';
+import { SiGithub, SiLinkedin, SiGmail } from 'react-icons/si';
 
 export interface SocialType {
   type: string;
@@ -19,11 +19,11 @@ export const LINKEDIN_SOCIAL: SocialType = {
   link: 'https://www.linkedin.com/in/jin-hao-l',
 };
 
-export const INSTA_SOCIAL: SocialType = {
-  type: 'instagram',
-  Icon: SiInstagram,
-  link: 'https://www.instagram.com/jin_haooo',
-};
+// export const INSTA_SOCIAL: SocialType = {
+//   type: 'instagram',
+//   Icon: SiInstagram,
+//   link: 'https://www.instagram.com/jin_haooo',
+// };
 
 export const MAIL_SOCIAL: SocialType = {
   type: 'mail',
@@ -31,5 +31,5 @@ export const MAIL_SOCIAL: SocialType = {
   link: 'mailto:limjinhao@gmail.com',
 };
 
-const SOCIALS: SocialType[] = [GITHUB_SOCIAL, LINKEDIN_SOCIAL, INSTA_SOCIAL, MAIL_SOCIAL];
+const SOCIALS: SocialType[] = [GITHUB_SOCIAL, LINKEDIN_SOCIAL, MAIL_SOCIAL];
 export default SOCIALS;
