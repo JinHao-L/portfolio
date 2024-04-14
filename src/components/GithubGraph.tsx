2f0041665bfcb856748973b5c28b1bb6a26fd6e9
JinHao-L <limjinhao@gmail.com>
2024-04-14 13:17:39 +0800

    feat: enhance react-github-calendar usage

--- src/components/GithubGraph.tsx
+++ src/components/GithubGraph.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import GitHubCalendar from 'react-github-calendar';
+import GitHubCalendar, { type ColorScale } from 'react-github-calendar';
 
 import SubSectionHeader from './SubSectionHeader';
+import './index.css';
 
 const GithubGraph: React.FC = () => {
-  const colourTheme = {
-    level4: '#ffffff',
-    level3: '#d7dddf',
-    level2: '#9baaae',
-    level1: '#72888e',
-    level0: 'transparent',
-  };
+  const colourTheme: ColorScale = ['rgba(0,0,0,0)', '#72888e', '#9baaae', '#d7dddf', '#ffffff'];
   return (
-    <div className="space-y-3">
-      <SubSectionHeader>My coding frequency:</SubSectionHeader>
+    <div className="space-y-3 flex flex-col overflow-hidden githubGraph">
+      <SubSectionHeader>My GitHub coding frequency:</SubSectionHeader>
       <GitHubCalendar
         username="jinhao-l"
         blockSize={15}
         blockMargin={5}
-        theme={colourTheme}
+        colorScheme="dark"
+        theme={{ dark: colourTheme }}
         fontSize={18}
+        throwOnError={false}
+        style={{ width: 'fit-content' }}
       />
     </div>
   );
